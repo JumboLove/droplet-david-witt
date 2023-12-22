a87e00b77c81fd66a32f53c39b29dd9e7c722732
David Witt <midknight55@gmail.com>
2023-12-22 09:45:01 -0800

    Refactored to use a reducer instead of states. Let's us clean up the React component and move logic outside of it

--- app/routes/_intersection+/index.tsx
+++ app/routes/_intersection+/index.tsx
@@ -1,5 +1,5 @@
 import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
-import { useCallback, useEffect, useState } from 'react'
+import { Reducer, useCallback, useEffect, useReducer, useState } from 'react'
 import { TrafficRoad } from '#app/components/intersection/traffic-road'
 import { type lightStatus } from '#app/components/intersection/types'
 import { Button } from '#app/components/ui/button'
@@ -13,48 +13,135 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	return null
 }
 
+// local types
+type trafficState = {
+	lanes: {
+		north: number[]
+		east: number[]
+		south: number[]
+		west: number[]
+	}
+	lights: {
+		north: lightStatus[]
+		east: lightStatus[]
+		south: lightStatus[]
+		west: lightStatus[]
+	}
+}
+
+type trafficActions =
+	| { type: 'tick' }
+	| { type: 'setLanes'; payload: trafficState['lanes'] }
+	| { type: 'setLights'; payload: trafficState['lights'] }
+
+const trafficReducer: Reducer<trafficState, trafficActions> = (
+	state,
+	action,
+) => {
+	switch (action.type) {
+		case 'tick': {
+			const newState = structuredClone(state)
+
+			newState.lanes = incrementCarsPerTick(newState.lanes)
+
+			return newState
+		}
+		case 'setLanes': {
+			// TODO
+			return state
+			break
+		}
+		case 'setLights': {
+			// TODO
+			return state
+		}
+	}
+}
+
+/**
+ * Inital state will be an empty intersection
+ * with greens in the east/west direction
+ * and flashing oranges for the left turns
+ */
+function initialState() {
+	const initalSate: trafficState = {
+		lanes: {
+			north: new Array(lanesCount).fill(0),
+			east: new Array(lanesCount).fill(0),
+			south: new Array(lanesCount).fill(0),
+			west: new Array(lanesCount).fill(0),
+		},
+		lights: {
+			north: new Array(lanesCount).fill({ color: 'r', time: 0 }),
+			east: new Array(lanesCount).fill(0).map((_v, i) => ({
+				color: i === 0 ? 'o' : 'g',
+				time: 0,
+			})),
+			south: new Array(lanesCount).fill({ color: 'r', time: 0 }),
+			west: new Array(lanesCount).fill(0).map((_v, i) => ({
+				color: i === 0 ? 'o' : 'g',
+				time: 0,
+			})),
+		},
+	}
+
+	return initalSate
+}
+
+/**
+ * Add cars to the intersection
+ * This function mutates the input object
+ */
+function incrementCarsPerTick(lanes: trafficState['lanes']) {
+	// add 0-4 cars per tick
+	const incrementAmount = Math.floor(Math.random() * 5)
+
+	if (incrementAmount === 0) {
+		return lanes
+	}
+
+	for (let i = 0; i < incrementAmount; i++) {
+		// randomize the direction and lane number
+		const direction = Math.floor(Math.random() * 4)
+		const lane = Math.floor(Math.random() * lanesCount)
+
+		switch (direction) {
+			// North
+			case 0: {
+				lanes.north[lane] += 1
+				break
+			}
+			// East
+			case 1: {
+				lanes.east[lane] += 1
+				break
+			}
+			// South
+			case 2: {
+				lanes.south[lane] += 1
+				break
+			}
+			// West
+			case 3: {
+				lanes.west[lane] += 1
+				break
+			}
+			default:
+				console.log('off by one error in incrementCarsPerTick')
+				break
+		}
+	}
+
+	return lanes
+}
+
 export default function Index() {
 	// Controls
 	const [simulationStatus, setSimulationStatus] = useState<
 		'stoppped' | 'playing' | 'paused'
 	>('stoppped')
 
-	// Traffic data
-	const [northLanes, setNorthLanes] = useState<number[]>(
-		new Array(lanesCount).fill(0),
-	)
-	const [southLanes, setSouthLanes] = useState<number[]>(
-		new Array(lanesCount).fill(0),
-	)
-	const [eastLanes, setEastLanes] = useState<number[]>(
-		new Array(lanesCount).fill(0),
-	)
-	const [westLanes, setWestLanes] = useState<number[]>(
-		new Array(lanesCount).fill(0),
-	)
-
-	// Light data
-	const [northLights, setNorthLights] = useState<lightStatus[]>(
-		new Array(lanesCount).fill({ color: 'r', time: 0 }),
-	)
-
-	const [southLights, setSouthLights] = useState<lightStatus[]>(
-		new Array(lanesCount).fill({ color: 'r', time: 0 }),
-	)
-
-	const [eastLights, setEastLights] = useState<lightStatus[]>(() => {
-		return new Array(lanesCount).fill(0).map((_v, i) => ({
-			color: i === 0 ? 'o' : 'g',
-			time: 0,
-		}))
-	})
-
-	const [westLights, setWestLights] = useState<lightStatus[]>(() => {
-		return new Array(lanesCount).fill(0).map((_v, i) => ({
-			color: i === 0 ? 'o' : 'g',
-			time: 0,
-		}))
-	})
+	const [state, dispatch] = useReducer(trafficReducer, initialState())
 
 	function toggleSimulation() {
 		switch (simulationStatus) {
@@ -65,83 +152,15 @@ export default function Index() {
 			case 'stoppped':
 				setSimulationStatus('playing')
 				break
-			default:
-				// Error if reaching default case
-				break
 		}
 	}
-
-	const incrementCarsPerTick = useCallback(() => {
-		// add 0-4 cars per tick
-		const incrementAmount = Math.floor(Math.random() * 5)
-		console.log(`Adding ${incrementAmount} cars`)
-
-		if (incrementAmount === 0) {
-			return
-		}
-
-		const updatedNorthLanes = [...northLanes]
-		const updatedEastLanes = [...eastLanes]
-		const updatedSouthLanes = [...southLanes]
-		const updatedWestLanes = [...westLanes]
-
-		for (let i = 0; i < incrementAmount; i++) {
-			// randomize the direction and lane number
-			const direction = Math.floor(Math.random() * 4)
-			const lane = Math.floor(Math.random() * lanesCount)
-
-			switch (direction) {
-				// North
-				case 0: {
-					updatedNorthLanes[lane] += 1
-					break
-				}
-				// East
-				case 1: {
-					updatedEastLanes[lane] += 1
-					break
-				}
-				// South
-				case 2: {
-					updatedSouthLanes[lane] += 1
-					break
-				}
-				// West
-				case 3: {
-					updatedWestLanes[lane] += 1
-					break
-				}
-				default:
-					console.log('off by one error')
-					break
-			}
-		}
-
-		// Set new lane values
-		setNorthLanes(updatedNorthLanes)
-		setEastLanes(updatedEastLanes)
-		setSouthLanes(updatedSouthLanes)
-		setWestLanes(updatedWestLanes)
-	}, [eastLanes, northLanes, southLanes, westLanes])
-
-	// Runs on every tick
-	const updateState = useCallback(() => {
-		console.log('running update State')
-
-		// generate new cars randomly
-		incrementCarsPerTick()
-
-		// process next tick position for cars with a green light
-
-		// set the next state of the lights
-	}, [incrementCarsPerTick])
 
 	// tick function operates outside of React events
 	useEffect(() => {
 		let intervalId: ReturnType<typeof setTimeout> | null = null
 		switch (simulationStatus) {
 			case 'playing':
-				intervalId = setInterval(updateState, 1000)
+				intervalId = setInterval(() => dispatch({ type: 'tick' }), 1000)
 				break
 			case 'paused':
 			case 'stoppped':
@@ -155,7 +174,7 @@ export default function Index() {
 				clearInterval(intervalId)
 			}
 		}
-	}, [simulationStatus, updateState])
+	}, [simulationStatus])
 
 	return (
 		<main className="container relative min-h-screen">
@@ -171,33 +190,33 @@ export default function Index() {
 				<div className="grid h-full grid-cols-3 grid-rows-3">
 					<div className="col-start-2 row-start-1">
 						<TrafficRoad
-							lanesState={northLanes}
-							lightsState={northLights}
+							lanesState={state.lanes.north}
+							lightsState={state.lights.north}
 							label="North"
 							className="rotate-180"
 						/>
 					</div>
-					<div className="col-start-1 row-start-2">
-						<TrafficRoad
-							lanesState={westLanes}
-							lightsState={westLights}
-							label="West"
-							className="rotate-90"
-						/>
-					</div>
 					<div className="col-start-3 row-start-2">
 						<TrafficRoad
-							lanesState={eastLanes}
-							lightsState={eastLights}
+							lanesState={state.lanes.east}
+							lightsState={state.lights.east}
 							label="East"
 							className="-rotate-90"
 						/>
 					</div>
 					<div className="col-start-2 row-start-3">
 						<TrafficRoad
-							lanesState={southLanes}
-							lightsState={southLights}
+							lanesState={state.lanes.south}
+							lightsState={state.lights.south}
 							label="South"
+						/>
+					</div>
+					<div className="col-start-1 row-start-2">
+						<TrafficRoad
+							lanesState={state.lanes.west}
+							lightsState={state.lights.west}
+							label="West"
+							className="rotate-90"
 						/>
 					</div>
 				</div>
@@ -214,8 +233,9 @@ function ToDoList() {
 				<li>Setup state manager</li>
 				<li>Setup control inputs - reset</li>
 				<li>Setup configurations - light times, car spawn rate</li>
-				<li>Spawn cars, assign to lanes</li>
-				<li>Setup tick logic</li>
+				<li>Decrement cars in lanes on their green light</li>
+				<li>Setup timers for lights</li>
+				<li>Setup triggers to be a bit smarter</li>
 				<li>Setup pedestrian signal and light</li>
 			</ul>
 		</details>
